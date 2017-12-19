0b7bcf2dc4ceead89a53c142056d6d786eadb85b
Kayleigh <myriamkayleigh@gmail.com>
2017-12-18 23:36:15 -0600

    Change syntax

--- CaesarCipher/CaesarCipher.rb
+++ CaesarCipher/CaesarCipher.rb
@@ -1,27 +1,21 @@
-alpha_array = ["A", "B", "C", "D", "E",
-  "F", "G", "H", "I", "J", "K",
-  "L", "M", "N", "O", "P",
-  "Q", "R", "S", "T", "U",
-  "V", "W", "X", "Y", "Z"]
+alpha_array = ('A'..'Z').to_a
 
 puts "What phrase do you want to cipher using the Caesar Cipher Method?"
-saying = gets.chomp
+phrase = gets.chomp
 puts "Choose a number:"
 shifting_num = gets.chomp.to_i
-saying_length = saying.length
-ciphered_saying = ""
+phrase_length = phrase.length
+ciphered_phrase = ""
 
-for i in (0...saying_length)
-  letter = saying[i]
+(0...phrase_length).each do |i|
+  letter = phrase[i]
   current_index_of_s = alpha_array.index(letter.to_s.upcase)
-  new_index_of_s = current_index_of_s.to_i + shifting_num
-  if new_index_of_s > 25
-    new_index_of_s = new_index_of_s % 26
-  end
+  new_index_of_s = (current_index_of_s.to_i + shifting_num) % 26
   if alpha_array.include?(letter.to_s.upcase)
-    ciphered_saying << alpha_array[new_index_of_s].to_s
+    ciphered_phrase << alpha_array[new_index_of_s].to_s
   else
-    ciphered_saying << letter
+    ciphered_phrase << letter
   end
 end
-puts ciphered_saying.capitalize
+
+puts ciphered_phrase.capitalize
