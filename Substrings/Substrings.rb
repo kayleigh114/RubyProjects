ce7a7ad1e90b998d674dbf9077d4dec399571c07
Kayleigh <myriamkayleigh@gmail.com>
2017-12-24 20:03:27 -0600

    Change to account for Ruby Style Conventions

--- Substrings/Substrings.rb
+++ Substrings/Substrings.rb
@@ -1,19 +1,21 @@
 =begin
   defaults for test
-  dictionary = ["hi", "the", "be", "tea", "need", "eat", "aid", "he", "we",
+   = ["hi", "the", "be", "tea", "need", "eat", "aid", "he", "we",
     "air", "bear", "ear", "nail", "ate", "wait", "mate"]
 
   string = "Have a great time in the sun!"
+
+  https://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks?ref=lnav
 =end
 
 def substrings(string, dictionary)
   array_of_string = string.downcase.split(" ")
   all_included_substrings = Hash.new
   all_included_substrings.default = 0
-  for inside_string in dictionary
-    for outside_string in array_of_string
-      if outside_string.include?(inside_string)
-        all_included_substrings[inside_string] = all_included_substrings[inside_string] + 1
+  dictionary.each do |dict_word|
+    array_of_string.each do |input_word|
+      if input_word.include?(dict_word)
+        all_included_substrings[dict_word] = all_included_substrings[dict_word] + 1
       end
     end
   end
