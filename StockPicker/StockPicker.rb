8d492c34620187fd02bfd9192586934f04756aea
Kayleigh <myriamkayleigh@gmail.com>
2017-12-24 19:47:21 -0600

    Change to account for Ruby Style Conventions

--- StockPicker/StockPicker.rb
+++ StockPicker/StockPicker.rb
@@ -1,25 +1,26 @@
 =begin
- highest price chosen must have a lower price before to buy
- therefore, index of max must be greater than min's index
+ input array represents stock prices for each day
 
- also, right minus left should be biggest difference
+ output should be a pair of days, 
+ where buying and selling has the biggest profit
+ https://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks?ref=lnav
 =end
 
 def array_best_days(array)
   current_biggest_diff = 0
   best_days_array = []
-  array.each {
-    |left_number|
-    array.each {
-      |right_number|
-      if array.index(left_number) < array.index(right_number)
-        if current_biggest_diff < (right_number - left_number)
-          current_biggest_diff = right_number - left_number
-          best_days_array = [array.index(left_number) + 1, array.index(right_number) + 1]
-        end
+  array.each_with_index do |left_number, buy|
+    array[buy+1..-1].each_with_index do |right_number, sell|
+      puts "buy: #{buy} and sell: #{sell}"
+      puts "left: #{left_number} and right: #{right_number}"
+
+      difference = right_number - left_number
+      if current_biggest_diff < difference
+        current_biggest_diff = difference
+        best_days_array = [buy + 1, sell + buy + 2]
       end
-    }
-  }
+    end
+  end
   print best_days_array, "\n"
 end
 
