b1bc72f2195ca3e5f2e6188bf478c7e1e54b633b
Kayleigh <myriamkayleigh@gmail.com>
2017-12-27 15:10:09 -0600

    Add readme file Change Caesar Cipher file to be more easily ran through command line

--- CaesarCipher/CaesarCipher.rb
+++ CaesarCipher/CaesarCipher.rb
@@ -1,21 +1,16 @@
-alpha_array = ('A'..'Z').to_a
-
-puts "What phrase do you want to cipher using the Caesar Cipher Method?"
-phrase = gets.chomp
-puts "Choose a number:"
-shifting_num = gets.chomp.to_i
-phrase_length = phrase.length
-ciphered_phrase = ""
-
-(0...phrase_length).each do |i|
-  letter = phrase[i]
-  current_index_of_s = alpha_array.index(letter.to_s.upcase)
-  new_index_of_s = (current_index_of_s.to_i + shifting_num) % 26
-  if alpha_array.include?(letter.to_s.upcase)
-    ciphered_phrase << alpha_array[new_index_of_s].to_s
-  else
-    ciphered_phrase << letter
+def caesar_cipher(phrase, shifting_num)
+  alpha_array = ('A'..'Z').to_a
+  phrase_length = phrase.length
+  ciphered_phrase = ""
+  (0...phrase_length).each do |i|
+    letter = phrase[i]
+    current_index_of_s = alpha_array.index(letter.to_s.upcase)
+    new_index_of_s = (current_index_of_s.to_i + shifting_num) % 26
+    if alpha_array.include?(letter.to_s.upcase)
+      ciphered_phrase << alpha_array[new_index_of_s].to_s
+    else
+      ciphered_phrase << letter
+    end
   end
+  puts ciphered_phrase.capitalize
 end
-
-puts ciphered_phrase.capitalize
---
+++ CaesarCipher/readme.md
@@ -0,0 +1,13 @@
+Download file onto computer.
+To run, use command line.
+`cd` to directory with CaesarCipher.rb in it.
+
+```
+irb
+```
+
+Within the Interactive Ruby Shell:
+```
+require "./CaesarCipher.rb"
+caesar_cipher("Hey there a!", 1)
+```
