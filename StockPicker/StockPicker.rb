37ae4f6cffd62ce29e9d450b111271771ecfcdd6
Kayleigh <myriamkayleigh@gmail.com>
2017-12-27 18:50:48 -0600

    Remove comments

--- StockPicker/StockPicker.rb
+++ StockPicker/StockPicker.rb
@@ -1,19 +1,8 @@
-=begin
- input array represents stock prices for each day
-
- output should be a pair of days, 
- where buying and selling has the biggest profit
- https://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks?ref=lnav
-=end
-
 def array_best_days(array)
   current_biggest_diff = 0
   best_days_array = []
   array.each_with_index do |left_number, buy|
     array[buy+1..-1].each_with_index do |right_number, sell|
-      puts "buy: #{buy} and sell: #{sell}"
-      puts "left: #{left_number} and right: #{right_number}"
-
       difference = right_number - left_number
       if current_biggest_diff < difference
         current_biggest_diff = difference
@@ -21,12 +10,5 @@ def array_best_days(array)
       end
     end
   end
-  print best_days_array, "\n"
+  return best_days_array
 end
-
-=begin
-  TEST
-  in file's same directory open irb
-  require "./StockPicker.rb"
-  call array_best_days(any_array_with_positive_numbers)
-=end
